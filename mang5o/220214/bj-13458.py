600216840a61216f61a0594e144e2ff3832db27c
mang5o <mangocode0611@gmail.com>
2022-02-08 02:55:02 +0900

    solve : bj 13458&9095 pg 17677

---
+++ mang5o/220214/bj-13458.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/13458
+import math
+N = int(input())
+persons = list(map(int, input().split()))
+B, C = map(int, input().split())
+sum_num = len(persons)
+for i in range(len(persons)):
+    persons[i] = max(persons[i]-B, 0)
+for i in persons:
+    sum_num += math.ceil(i / C)
+print(sum_num)
