109e3bf3522011542600ac8b9d5d65e2739e051e
hwjeon <hw3652@naver.com>
2022-09-06 20:34:47 +0900

    220906-hwjeon

---
+++ hwjeon/220906/boj9372.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		int n, m;
+		cin >> n >> m;
+		for (int j = 0; j < m; j++)
+		{
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << n - 1 << "\n";
+	}
+}
